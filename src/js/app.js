0c688abb7bcbf65f2c3f9aa4b5aa930d67d92197
Victor Gonzalez <liseezbusiness@gmail.com>
2023-10-25 16:36:45 +0200

    mejora parte proyectos

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 function temaOscuro() {
   document.querySelector("body").setAttribute("data-bs-theme", "dark");
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
-
 }
 
 function temaClaro() {
