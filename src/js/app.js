16b473ce1bd3839c09520cdd0c5eda21374fb1f5
Victor Gonzalez <liseezbusiness@gmail.com>
2023-10-25 13:44:55 +0200

    modo oscuro - claro

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+function temaOscuro() {
+  document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+
+}
+
+function temaClaro() {
+  document.querySelector("body").setAttribute("data-bs-theme", "light");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+}
+
+function cambiarTema() {
+  document.querySelector("body").getAttribute("data-bs-theme") === "light" ?
+    temaOscuro() : temaClaro();
+}
